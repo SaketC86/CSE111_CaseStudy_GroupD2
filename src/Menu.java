7497ff72ddeae14cecc3267945f483bba1383477
SaketC86 <saketchidire@gmail.com>
2026-05-07 00:34:28 +0530

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -1,40 +1,40 @@
 package OnlineFoodSystem;
+import java.util.ArrayList;
 
 public class Menu {
+
     private String itemName;
-    private int price;
+    private double price;
     private String category;
     private boolean availability;
 
-    public Menu(String itemName, int price, String category, boolean availability) {
+    public Menu(String itemName, double price,
+                String category, boolean availability) {
         this.itemName = itemName;
         this.price = price;
         this.category = category;
         this.availability = availability;
     }
 
-    public String displayItem(){
+    public String displayItem() {
         System.out.println("Item: " + itemName);
         System.out.println("Price: " + price);
         System.out.println("Category: " + category);
         System.out.println("Available: " + availability);
         return "Shown";
     }
-    public String updateAvailability(boolean availability) {
-        this.availability = availability;
-        return "Availability updated";
+
+    public static void displayMenu(ArrayList<Menu> menuList) {
+        for (int i = 0; i < menuList.size(); i++) {
+            Menu m = menuList.get(i);
+            System.out.println((i + 1) + ". " + m.getItemName() + " - ₹" + m.getPrice());
+        }
     }
 
-    public double updatePrice(double price) {
-        this.price = (int) price;
-        return price;
-    }
-
-    public String getItemName() {
-        return itemName;
-    }
-
-    public int getPrice() {
-        return price;
-    }
+    public String getItemName(){
+    	return itemName;
+    	}
+    public double getPrice(){
+    	return price;
+    	}
 }
